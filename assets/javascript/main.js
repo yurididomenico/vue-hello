415e05b5249a2c924ef64ae8eec4662c3904bf8a
yurididomenico <yurididomenico93@gmail.com>
2022-11-10 15:39:47 +0100

    Vue: titolo aggiunto dinamicamente

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,9 +2,21 @@
 
 console.log("YDD");
 
+/*
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+*/
 
-
-
+var app = new Vue
+(
+    {
+        el: '#root',
+        data: 
+        {
+            titolo: "Titolo"
+        }
+        
+    }
+);
 
 
 
